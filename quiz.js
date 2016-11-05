5f7b2b48ef2bc6a5fa44c184d362b3c2e1ff7d57
Abby Moore <abbyleighanne@gmail.com>
2016-11-04 21:30:59 -0500

    spaces added

--- quiz.js
+++ quiz.js
@@ -1,72 +1,37 @@
-
-
-
-/*---- GET / TEST THE INPUT ------ */
-
-// function testInput(heightInput, charInput) {
-// 	console.log("It's working! ", heightInput, charInput);
-// 	console.log(input.height, input.character)
-// }
-
 function checkInput () {
-	var heightInput = document.getElementById("heightInput").value;
-	var charInput = document.getElementById("charInput").value;
+	var getTree = { 
+		height: document.getElementById("heightInput").value,
+		character: document.getElementById("charInput").value
+		};
 	
 	 if (heightInput && charInput){
-
-		beautifulTree(heightInput, charInput);
-	
-	 } else {
-			
-			alert("Please enter a height & character.")
+		beautifulTree(getTree);
+	 } else {	
+		alert("Please enter a height & character.");
 	 }
 }
 
-submit.addEventListener("click", checkInput, charInput);
 
-/*------------------------*/
-
-
-
-function beautifulTree (heightInput, charInput) {
-
-for (var i = 0; i < (heightInput * 2); i++) {
-	
-	var prettyTree = "";
-	prettyTree = charInput.repeat(i += 1);
-	
-	// var prettySpaces = "s";
-	// prettySpaces = prettySpaces.repeat(heightInput - i);
-
-	console.log(prettyTree);
-
-};
-
+function beautifulTree (getTree) {
+	for (i = 0; i < (getTree.height * 2); i++) {	
+		var prettyTree = "";
+		var prettySpaces = " ";		
+		prettyTree = getTree.character.repeat(i += 1);
+		prettySpaces = prettySpaces.repeat(getTree.height - i/2);
+		console.log(prettySpaces, prettyTree);
+	}
 }
 
+submit.addEventListener("click", checkInput);
 
-/*
+document.getElementById("heightInput").addEventListener("keyup", function(e) {
+ 	if (e.keyCode == 13) {
+	 checkInput();
+	}
+})
 
-i : index number
-h : height
-s : space
-* : character
-
-i: 0 | h: 3 | s: 2 | *: 1
-i: 1 | h: 3 | s: 1 | *: 3
-i: 2 | h: 3 | s: 0 | *: 5
-
-
-ss*    
-s***   
-*****  
-
-
-*/
-
-
-//console.log(theloop());
-
-
-
-
+document.getElementById("charInput").addEventListener("keyup", function(e) {
+ 	if (e.keyCode == 13) {
+	 checkInput();
+	}
+})
